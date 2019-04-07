46e5058694f64a2a09738ab0df1ace9e7743be0a
jtaguar <juatagrei@gmail.com>
2019-04-07 17:47:14 +0200

    added functions login and logout in auth.service.ts

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -6,18 +6,51 @@ import { headersToString } from 'selenium-webdriver/http';
 import { Actor } from '../models/actor.model';
 import {  HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { MessageService } from 'src/app/services/message.service';
 
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class AuthService {
+
+  constructor(private authService: AuthService,
+    private messageService: MessageService) {
+    }
+
   registerUser(value: any) {
     throw new Error("Method not implemented.");
   }
   fireAuth: any;
+
   getRole(): any {
     throw new Error('Method not implemented.');
   }
 
-  constructor() { }
+  login (email: string, password: string) {
+    return new Promise<any>((resolve, reject) => {
+      this.fireAuth.auth.signInWithEmailAndPassword(email, password)
+        .then(data => {
+        this.messageService.notifyMessage('errorMessages.', 'messages.auth.login.correct');
+        resolve();
+        }).catch(error => {
+          this.messageService.notifyMessage ('errorMessages.' + error.code.replace(/\//gi, '.').replace(/\-/gi, '.'), 'Error on Login');
+        reject(error);
+        });
+      });
+  }
+
+  logout() {
+    return new Promise<any>((resolve, reject) => {
+      this.fireAuth.auth.signOut()
+        .then(_ => {
+          resolve();
+      }).catch(error => {
+        reject(error);
+        this.messageService.notifyMessage(error.code, 'Error on Logout.');
+      });
+    });
+}
+
 }
