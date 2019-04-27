5430fc3a5548a462881684c4384a4e8ad63c1822
jtaguar <juatagrei@gmail.com>
2019-04-27 19:14:17 +0200

    added not found component routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,6 +29,7 @@ import { DashboardComponent } from './components/dashboard/dashboard/dashboard.c
 import { ApplicationListComponent } from './components/application/application-list/application-list/application-list.component';
 import { ApplicationDisplayComponent } from './components/application/application-display/application-display/application-display.component';
 import { TermAndConditionsComponent } from './components/master/terms-and-conditions/term-and-conditions/term-and-conditions.component';
+import { NotFoundComponent } from './components/shared/not-found/not-found.component';
 
 // Initialize Firebase
 export const firebaseConfig = {
@@ -66,7 +67,8 @@ export function HttpLoaderFactory(http: HttpClient) {
     DashboardComponent,
     ApplicationListComponent,
     ApplicationDisplayComponent,
-    TermAndConditionsComponent
+    TermAndConditionsComponent,
+    NotFoundComponent
 
   ],
   imports: [
