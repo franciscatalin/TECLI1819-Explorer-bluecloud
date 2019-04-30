ce511aeb9e05342b68d4f55db550e249c1b30104
jtaguar <juatagrei@gmail.com>
2019-04-30 16:33:49 +0200

    guards, routes, not-found, denied-access terms and condition and header

--- src/app/components/master/header/header.component.ts
+++ src/app/components/master/header/header.component.ts
@@ -3,6 +3,7 @@ import { AuthService } from '../../../services/auth.service';
 import { MessageService } from 'src/app/services/message.service';
 import { TranslatableComponent } from '../../shared/translatable/translatable.component'
 import { TranslateService } from '@ngx-translate/core';
+import { Actor } from '../../../models/actor.model';
 
 
 @Component({
@@ -11,6 +12,10 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent extends TranslatableComponent implements OnInit {
+
+  private currentActor: Actor;
+  private userLoggedIn: boolean;
+  private activeRole = 'anonymous';
 
   constructor(private translateService: TranslateService,
     private messageService: MessageService,
@@ -22,12 +27,30 @@ export class HeaderComponent extends TranslatableComponent implements OnInit {
     super.changeLanguage(language);
   }
 
+  // Cuando el servicio de login me diga que un usuario se ha logueado se ejecutará este trozo de código ya que estamos escuchando
   ngOnInit() {
+    this.authservice.userLoggedIn.subscribe((loggedIn: boolean) => {
+      // Si el usuario está logueado
+      if (loggedIn) {
+        // Le preguntamos al servicio de autenticación por el usuario que se acaba de loguear en la aplicación
+        this.currentActor = this.authservice.getCurrentActor();
+        // Obtenemos el rol activo, que es el valor que tendremos que comprobar
+        this.activeRole = this.currentActor.role.toString();
+        // En este caso no hay usuario logueado 
+      } else {
+        this.activeRole = 'anonymous';
+        this.currentActor = null;
+      }
+    });
   }
 
+  // Cuando el actor hace click en logout tengo que llamar al servicio y actualizar el header para mostrar que se ha salido del sistema
   logout() {
     this.authservice.logout()
       .then(_ => {
+        // Cuando todo ha ido correcto, el actor actual pasa a ser anónimo
+        this.activeRole = 'anonymous';
+        this.currentActor = null;
         this.messageService.notifyMessage('messages.auth.logout', 'alert alert-success');
       })
       .catch(error => {
---
+++ src/app/guards/actor-role.guard.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ActorRoleGuard implements CanActivate {
+
+  constructor(private authService: AuthService, private router: Router) {
+  }
+
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    return new Promise ((resolve, reject) => {
+      // Con expectedRole guardo el tipo de rol que puede acceder a cada ruta concreta y que lo recibo como parámetro
+      const expectedRole = next.data.expectedRole;
+      // Dentro del servicio auth.service tengo un método que me devuelve el actor actual logueado y lo guardo en currentActor
+      const currentActor = this.authService.getCurrentActor();
+      let result = false;
+      // Si currentActor = true significa que tenemos un actor logueado, es decir, el actor actual no es anónimo
+      if (currentActor) {
+        // Vemos cual es el rol activo del actor actual y lo guardamos en activeRole
+        const activeRole = new RegExp(currentActor.role.toString(), 'i');
+        // Si el rol que puede ejecutar la accion está contenido en el activeRole, significa que el actor actual logueado tiene ese permiso
+        if (expectedRole.search(activeRole) !== -1 ) {
+          // Con result = true le estaríamos concediendo el acceso al usuario a la ruta
+          result = true;
+        } else {
+          // Enrutamos al usuario hacia el componente de acceso denegado
+          this.router.navigate(['denied-access'], { queryParams: {previousURL: state.url }});
+        }
+        // Resolvemos la promesa
+        resolve(result);
+      } else {
+        // Si currentActor = false, significa que no tenemos un actor logueado, es decir, el usuario es anónimo.
+        // Si el expectedRole puede ser anónimo entonces el actor actual también tendrá permiso para acceder a la ruta
+        if (expectedRole.indexOf('anonymous') !== -1 ) {
+          // Con result = true le estaríamos concediendo el acceso al usuario a la ruta
+          result = true;
+        } else {
+          // En este último caso el usuario es anónimo, pero no puede ser anónimo para acceder a la ruta que está intentando entrar
+          // Lo que hacemos es redirigir al usuario a la vista de login, y si el usuario hace login con el rol adecuado
+          // lo redirigimos a la ruta a la que quería acceder, para eso guardamos dentro de returnUrl esa dirección a la que quería acceder
+          this.router.navigate(['login'], { queryParams: { returnUrl: state.url }});
+        }
+        resolve(result);
+      }
+    });
+  }
+}
