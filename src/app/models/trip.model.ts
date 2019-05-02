cae84cbabe8af1369891456b89669d749a4da6dc
Chanell13 <45261718+Chanell13@users.noreply.github.com>
2019-05-02 17:17:45 +0200

    Fix Display and List trip

--- src/app/models/trip.model.ts
+++ src/app/models/trip.model.ts
@@ -3,12 +3,15 @@ import { Actor } from './actor.model';
 
 export class Trip extends Entity{
    // Administrator: Actor;
+  
     ticker: String;
+    detalles: String;
    // cancelled_reason: String;
     title: String;
    // cancelationMoment: String;
     description: String;
     price: Number;
+    picture: String;
     //list_requirements: String [];
     //status: String;
     //date_start: Date;
