5914662cdbc61ca1ea16f5e05e0859af79b3d207
Chanell13 <45261718+Chanell13@users.noreply.github.com>
2019-05-03 19:49:06 +0200

    add translate json

--- src/app/models/trip.model.ts
+++ src/app/models/trip.model.ts
@@ -2,21 +2,20 @@ import { Entity } from './Entity.model';
 
 
 export class Trip extends Entity{
-   // Administrator: Actor;
+  
   
     ticker: String;
     detalles: String;
-   // cancelled_reason: String;
+    cancelled_reason: String;
     title: String;
-   // cancelationMoment: String;
+    cancelationMoment: String;
     description: String;
     price: Number;
     picture: String;
-    //list_requirements: String [];
-    //status: String;
-    //date_start: Date;
-    //date_end: Date;
-   // published: Boolean;
-   // picture: String [];
-   // created: Date;
+    list_requirements: String [];
+    status: String;
+    date_start: Date;
+    date_end: Date;
+    published: Boolean;
+    created: Date;
 }
