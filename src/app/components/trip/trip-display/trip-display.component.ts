b8139955cdb3f76f12dda21129304d6afbb5d8e6
Chanell13 <45261718+Chanell13@users.noreply.github.com>
2019-05-23 17:23:02 +0200

    Add filter

--- src/app/components/trip/trip-display/trip-display.component.ts
+++ src/app/components/trip/trip-display/trip-display.component.ts
@@ -16,25 +16,25 @@ export class TripDisplayComponent extends TranslatableComponent implements OnIni
   id: string;
 
   constructor(private tripservice: TripService,
-     private router: Router, private route: ActivatedRoute,
-     private translateservice: TranslateService) {
-      super (translateservice);
-     }
+    private router: Router, private route: ActivatedRoute,
+    private translateservice: TranslateService) {
+    super(translateservice);
+  }
 
   ngOnInit() {
     // A partir del listado de trips, el usuario selecciona un viaje que tiene un id concreto que se pasa por la URL y que aquí recuperamos
     this.id = this.route.snapshot.params['id'];
     // Una vez que ya tenemos el id, podemos usarlo para recuperar el trip completo
     this.tripservice.getTrip(this.id)
-    // Las llamadas a los métodos de los servicios como suelen generar llamadas a backend no devuelven el objeto sino que devuelven promesas
-    // De manera que cuando se resuelva la promesa, lo que me devuelve el método "getTrip" es un trip que se guarda en la variable trip
-    .then ((val) => {
-      this.trip = val;
-      console.log ('Trip id:' + this.trip.id);
-    })
-    .catch((err) => {
-      console.error (err);
+      // Las llamadas a los métodos de los servicios como suelen generar llamadas a backend no devuelven el objeto sino que devuelven promesas
+      // De manera que cuando se resuelva la promesa, lo que me devuelve el método "getTrip" es un trip que se guarda en la variable trip
+      .then((val) => {
+        this.trip = val;
+        console.log('Trip id:' + this.trip.id);
+      })
+      .catch((err) => {
+        console.error(err);
+      }
+      );
   }
-  );
-}
 }
